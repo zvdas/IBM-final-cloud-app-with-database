a482208f7edc8b7f859847cb55c3ba6c0142ed68
zvdas <rodriguezzude@gmail.com>
2022-12-18 01:34:27 -0500

    login successful cannot add data

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.ModelAdmin):
+    fields = ['question_text', 'question_grade']
 
+class ChoiceInline(admin.ModelAdmin):
+    fields = ['option']
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -28,3 +31,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
